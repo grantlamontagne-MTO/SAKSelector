429d2592ef5b31ac019af6d008d4b7583c3a27b0
grantlamontagne-MTO <grant.lamontagne@gmail.com>
2026-07-14 01:57:51 -0300

    Replace Jinja2Templates import from fastapi to starlette

--- app/main.py
+++ app/main.py
@@ -1,19 +1,17 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+from starlette.templating import Jinja2Templates
 
 app = FastAPI()
 
-# Serve static files (CSS, JS, images)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Template engine
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def landing_page(request: Request):
     return templates.TemplateResponse(
-        name="index.html",
-        context={"request": request}
+        "index.html",
+        {"request": request}
     )
